1048255e16e2e9c6fb643be67afdffd752f75a0a
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-04-17 15:54:53 +0000

    Added Navbar

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <nav className="navbar navbar-expand-lg sticky-top bg-dark ">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBar" aria-controls="navbarBar" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarBar">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Services</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                        <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
